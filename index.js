6fdf6a0288913175e2d989effa4932087ae69e91
Turjo-abhishek <turjoabhishek@gmail.com>
2022-10-26 22:53:47 +0600

    made a new json file for dynamic route to checkout page

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = process.env.port || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
+const premium = require('./data/premium.json');
 
 app.get('/', (req, res) => {
   res.send('API Running')
 })
+app.get('/courses', (req, res) => {
+    res.send(courses)
+  });
+
+  app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find( course => course._id === id);
+    res.send(selectedCourse);
+  })
+  app.get('/premium/:id', (req, res) => {
+    const id = req.params.id;
+    const premiumCourse = premium.find( course => course._id === id);
+    res.send(premiumCourse);
+  })
 
 app.listen(port, () => {
   console.log(`learner's hub server running on port`, port);
